bd8e587ffd405169e45920e4a0b007a6b4d821bd
LiangYT <381518188@qq.com>
2020-01-07 23:25:56 +0800

    升级开发环境！

--- app/src/main/java/com/sinothk/view/image9/demo/style5/Image9ImgStyle5DemoMainActivity.java
+++ app/src/main/java/com/sinothk/view/image9/demo/style5/Image9ImgStyle5DemoMainActivity.java
@@ -24,15 +24,16 @@ public class Image9ImgStyle5DemoMainActivity extends AppCompatActivity {
 
         ArrayList<String> imageUrls = new ArrayList<>();
         imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=829517287,685236282&fm=173&app=49&f=JPEG?w=312&h=208&s=049A5C3253026147D6E89846030020F1");
-        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=431680344,3254176197&fm=173&app=49&f=JPEG?w=312&h=208&s=711B36D5001031DEC03001250300E042");
-        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1252006621,4016768871&fm=173&app=49&f=JPEG?w=312&h=208&s=9963DE144BB0448E924750D80300D0B8");
+//        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=431680344,3254176197&fm=173&app=49&f=JPEG?w=312&h=208&s=711B36D5001031DEC03001250300E042");
+//        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1252006621,4016768871&fm=173&app=49&f=JPEG?w=312&h=208&s=9963DE144BB0448E924750D80300D0B8");
 
-        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3996130137,1482805126&fm=173&app=49&f=JPEG?w=312&h=208&s=752A9455CC873149549F8DEF0300801B");
-        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=829517287,685236282&fm=173&app=49&f=JPEG?w=312&h=208&s=049A5C3253026147D6E89846030020F1");
-        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2831567826,680499950&fm=173&app=49&f=JPEG?w=312&h=208&s=BBBB4C81D4DB03C66EBC350C0300E040");
+//        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3996130137,1482805126&fm=173&app=49&f=JPEG?w=312&h=208&s=752A9455CC873149549F8DEF0300801B");
+//        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=829517287,685236282&fm=173&app=49&f=JPEG?w=312&h=208&s=049A5C3253026147D6E89846030020F1");
+//        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2831567826,680499950&fm=173&app=49&f=JPEG?w=312&h=208&s=BBBB4C81D4DB03C66EBC350C0300E040");
 
-        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1252006621,4016768871&fm=173&app=49&f=JPEG?w=312&h=208&s=9963DE144BB0448E924750D80300D0B8");
-        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3996130137,1482805126&fm=173&app=49&f=JPEG?w=312&h=208&s=752A9455CC873149549F8DEF0300801B");
+//        imageUrls.add("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1252006621,4016768871&fm=173&app=49&f=JPEG?w=312&h=208&s=9963DE144BB0448E924750D80300D0B8");
+//        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3996130137,1482805126&fm=173&app=49&f=JPEG?w=312&h=208&s=752A9455CC873149549F8DEF0300801B");
+//        imageUrls.add("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3996130137,1482805126&fm=173&app=49&f=JPEG?w=312&h=208&s=752A9455CC873149549F8DEF0300801B");
 
         NineImageView nineImageView = findViewById(R.id.multi_image);
         //set the picture url set
